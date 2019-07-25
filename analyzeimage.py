d1e4b127db0c7213851654e378210250d1d74e43
joegenius98 <jo112095@gmail.com>
2019-07-25 11:04:38 -0400

    Added in Hayden's code then modified it as neccessary.

--- analyzeimage.py
+++ analyzeimage.py
@@ -1,7 +1,9 @@
 
 
 def analyzeimage(old_image):
+	
 	# analyze the image 'old_image' here
 	# save the analyzed image to 'static/temp.jpg'
 	# return the path of the new image file
+
 	return "static/temp.jpg"
